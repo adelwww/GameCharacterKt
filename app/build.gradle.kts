c4ba948f358e044cf7b3353b432b1f5db3fc0839
adelwww <wsnezka@gmail.com>
2022-05-17 23:47:32 +0300

    3 dz + доп

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,18 +1,18 @@
 plugins {
+
+    kotlin("android")
+
     // Application
     id(libs.plugins.agp.application.get().pluginId)
 
-    // Kotlin
-    id("kotlin-android")
+    // Hilt
+    id(libs.plugins.hilt.android.get().pluginId)
 
     // Kapt
     id("kotlin-kapt")
 
     // Navigation SafeArgs
     id(libs.plugins.navigation.safeArgs.get().pluginId)
-
-    // Hilt
-    id(libs.plugins.hilt.android.get().pluginId)
 }
 
 android {
@@ -50,6 +50,61 @@ android {
 }
 
 dependencies {
+
+    implementation(project(":domain"))
+    implementation(project(":data"))
+
+    //Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.1")
+
+    //Core
+    implementation("androidx.core:core-ktx:1.7.0")
+
+    //AppCompat
+    implementation("androidx.appcompat:appcompat:1.4.1")
+
+    //Material Design Components
+    implementation("com.google.android.material:material:1.6.0")
+
+    //UI Components
+    implementation("androidx.constraintlayout:constraintlayout:2.1.3")
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
+
+    // Activity
+    val activity_version = "1.4.0"
+    implementation("androidx.activity:activity-ktx:$activity_version")
+
+    // fragment
+    val fragment_version = "1.4.1"
+
+    implementation("androidx.fragment:fragment-ktx:$fragment_version")
+
+    //viewBinding
+    implementation("com.github.kirich1409:viewbindingpropertydelegate-noreflection:1.5.6")
+
+    // Lifecycle
+    val lifecycle_version = "2.4.0"
+
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycle_version")
+
+    // | for ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
+
+    // ViewModel and LiveData
+    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+
+    //navigation
+    val nav_version = "2.3.5"
+    implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
+    implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
+
+    //Glide
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+
+    // Hilt
+    implementation(libs.hilt)
+    kapt("com.google.dagger:hilt-android-compiler:2.40")
 
     implementation(project(":domain"))
     implementation(project(":data"))
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "7.1.2" apply false
-    id("com.android.library") version "7.1.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.5.30" apply false
-    id("com.google.dagger.hilt.android") version "2.41" apply false
-    id("org.jetbrains.kotlin.jvm") version "1.5.30" apply false
+    alias(libs.plugins.agp.application) apply false
+    alias(libs.plugins.agp.library) apply false
+    alias(libs.plugins.kotlin.gradle) apply false
+    alias(libs.plugins.navigation.safeArgs) apply false
+    alias(libs.plugins.hilt.android) apply false
 }
 
 tasks.register("clean", Delete::class) {
